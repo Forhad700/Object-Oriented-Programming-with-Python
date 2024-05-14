ae96d567e0a4e4e295f8b5a425821f490fcdce90
Forhad <71933188+Forhad700@users.noreply.github.com>
2024-05-14 13:14:01 +0600

    Delete Basic OOP directory

--- Basic OOP/class_object.py
+++
@@ -1,9 +0,0 @@
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-
-c = Car("Mitsubishi", "Pajero", 2015)
-print("Car info..............")
-print(c.make, c.model, c.year)
